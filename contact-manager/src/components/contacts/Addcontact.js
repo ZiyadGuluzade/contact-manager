1f76851509f8e49bfb7f7f2092dc7c94479a44a9
Z.Guluzade <ziyad_guluzade@outlook.com>
2019-07-17 16:39:45 -0400

    created add-contact component

--- contact-manager/src/components/contacts/Addcontact.js
+++ contact-manager/src/components/contacts/Addcontact.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 
 class Addcontact extends Component {
+    state = {
+        name: '',
+        email: '',
+        phone: ''
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="card mb-3">
+                <div className="card-header">
+                    Add Contact
+                </div>
+                <div className="card-body">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Name..."
+                                name="name" />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Email..." />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone</label>
+                            <input
+                                type="text"
+                                name="phone"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Phone..." />
+                        </div>
+                        <input value="Add Contact" type="submit" 
+                            className="btn btn-light btn-block" />
+                    </form>
+                </div>
             </div>
         )
     }
